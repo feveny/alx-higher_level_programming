9082ec95453584a99fac02d656a5634e57b1ef5b
feveny <yfeven8@gmail.com>
2023-11-06 13:35:13 -0800

    error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+Replace an element in a list at a specific position without
+modifying the original list
+"""
+
+
 def new_in_list(my_list, idx, element):
-    new_list=[]
     if idx < 0 or idx > len(my_list) - 1:
-        return my_list
+        return my_list.copy()
     else:
-        for i in range(len(my_list)):
-            new_list.append(my_list[i])
+        new_list = my_list.copy()
         new_list[idx] = element
         return new_list
+
